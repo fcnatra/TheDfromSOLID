2aea3df9e770e0f2949a7c00c9ee47237890c5d9
frank.torres <frank.torres@ekasa.net>
2019-03-15 10:34:04 +0100

    Content Added to InputHub - not tested

--- TheDfromSOLID/InputHub.cs
+++ TheDfromSOLID/InputHub.cs
@@ -1,12 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Timers;
 
 namespace TheDfromSOLID
 {
-    internal class InputHub
+    internal class InputHub : IDisposable
     {
+        private Timer timer = new Timer();
+
+        public bool IsListening { get { return timer.Enabled; } }
+
+        public Interfaces.IConfiguration Configuration { get; set; }
+        public Interfaces.IHub Hub { get; set; }
+
+        public void StartListening()
+        {
+            CheckIAmNotAlreadyListening();
+
+            timer = new Timer { Interval = Configuration.ReadingIntervalInMs };
+            timer.Start();
+            timer.Elapsed += Timer_Elapsed;
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            ReadFromHub();
+        }
+
+        private void CheckIAmNotAlreadyListening()
+        {
+            if (timer.Enabled) throw new InvalidOperationException("This instance is already listening.");
+        }
+
+        internal void StopListening()
+        {
+            timer.Stop();
+            timer.Dispose();
+        }
+
+        public void Dispose()
+        {
+            StopListening();
+        }
     }
 }
--- TheDfromSOLID/Program.cs
+++ TheDfromSOLID/Program.cs
@@ -1,15 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using TheDfromSOLID.Interfaces;
 
 namespace TheDfromSOLID
 {
     class Program
     {
+        private static IConfiguration configuration;
+
         static void Main(string[] args)
         {
+            configuration = InitializeConfiguration();
+            InputHub inputHub = StartListeningOnInputHub();
+
+            PressAnyKeyToExit();
+
+            inputHub.StopListening();
+
+            Console.WriteLine("Program terminated.");
+        }
+
+        private static InputHub StartListeningOnInputHub()
+        {
+            var inputHub = new InputHub { Configuration = configuration };
+            inputHub.StartListening();
+            return inputHub;
+        }
+
+        private static IConfiguration InitializeConfiguration()
+        {
+            IConfiguration configuration = new Configuration();
+            configuration.UpdateInformation();
+            return configuration;
+        }
+
+        private static void PressAnyKeyToExit()
+        {
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
         }
     }
 }
